6998b4f8fdb6ff50bf8214fb8696e3b9aeb9a024
sedera0 <hei.sedera@gmail.com>
2026-03-12 23:06:17 +0300

    feat: google auth

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -1,8 +1,13 @@
 import * as express from "express";
 
 import { AccountController } from "@/controllers";
+import { GoogleAuthController } from "@/controllers/google-auth-controllers";
 
 export const authRouter = express.Router();
 
 authRouter.post("/sign-up", AccountController.signUp);
 authRouter.post("/sign-in", AccountController.signIn);
+
+authRouter.post("/google", GoogleAuthController.signInWithIdToken);
+
+authRouter.post("/google/callback", GoogleAuthController.signInWithCode);
